65dc23acf6037e4c2166bf750997470a50abb1b5
femailman <93400575+femailman@users.noreply.github.com>
2023-10-30 21:53:49 -0400

    Implement VehicleRackArea interactable types and corresponding commands

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -17,7 +17,7 @@ export type TownJoinResponse = {
   interactables: TypedInteractable[];
 }
 
-export type InteractableType = 'ConversationArea' | 'ViewingArea' | 'TicTacToeArea';
+export type InteractableType = 'ConversationArea' | 'ViewingArea' | 'TicTacToeArea' | 'VehicleRackArea';
 export interface Interactable {
   type: InteractableType;
   id: InteractableID;
@@ -66,6 +66,10 @@ export interface BoundingBox {
   width: number;
   height: number;
 };
+
+export interface VehicleRackArea extends Interactable {
+  vehicle?: Vehicle;
+}
 
 export interface ViewingArea extends Interactable {
   video?: string;
@@ -174,7 +178,7 @@ interface InteractableCommandBase {
   type: string;
 }
 
-export type InteractableCommand =  ViewingAreaUpdateCommand | JoinGameCommand | GameMoveCommand<TicTacToeMove> | LeaveGameCommand;
+export type InteractableCommand =  ViewingAreaUpdateCommand | JoinGameCommand | GameMoveCommand<TicTacToeMove> | LeaveGameCommand | EquipVehicleCommand | UnequipVehicleCommand;
 export interface ViewingAreaUpdateCommand  {
   type: 'ViewingAreaUpdate';
   update: ViewingArea;
@@ -191,11 +195,20 @@ export interface GameMoveCommand<MoveType> {
   gameID: GameInstanceID;
   move: MoveType;
 }
+export interface EquipVehicleCommand {
+	type: 'EquipVehicle';
+	vehicle: Vehicle;
+}
+export interface UnequipVehicleCommand {
+	type: 'UnequipVehicle';
+}
 export type InteractableCommandReturnType<CommandType extends InteractableCommand> = 
   CommandType extends JoinGameCommand ? { gameID: string}:
   CommandType extends ViewingAreaUpdateCommand ? undefined :
   CommandType extends GameMoveCommand<TicTacToeMove> ? undefined :
   CommandType extends LeaveGameCommand ? undefined :
+  CommandType extends EquipVehicleCommand ? undefined :
+  CommandType extends UnequipVehicleCommand ? undefined :
   never;
 
 export type InteractableCommandResponse<MessageType> = {
