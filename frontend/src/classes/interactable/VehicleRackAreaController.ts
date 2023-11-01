951faa89c4562b64e48006280816c99bc7728714
snavemail <98936310+snavemail@users.noreply.github.com>
2023-11-01 09:55:34 -0400

    Allow set vehicle to allow undefined for get command's return type to allow undefined

--- frontend/src/classes/interactable/VehicleRackAreaController.ts
+++ frontend/src/classes/interactable/VehicleRackAreaController.ts
@@ -1,4 +1,4 @@
-import { VehicleRackArea as VehicleRackAreaModel } from '../../types/CoveyTownSocket';
+import { Vehicle, VehicleRackArea as VehicleRackAreaModel } from '../../types/CoveyTownSocket';
 import InteractableAreaController, { BaseInteractableEventMap } from './InteractableAreaController';
 
 /**
@@ -7,54 +7,54 @@ import InteractableAreaController, { BaseInteractableEventMap } from './Interact
 export type VehicleRackAreaEvents = BaseInteractableEventMap & {
   /**
    * A vehicleChange event indicates that the vehicle selected for this vehicle rack area has changed.
-   * Must implement Vehicle type to 
-  */
+   * Must implement Vehicle type to
+   */
   vehicleChange: (vehicle: Vehicle | undefined) => void;
 };
 
 export default class VehicleRackAreaController extends InteractableAreaController<
-VehicleRackAreaEvents, 
-VehicleRackAreaModel
+  VehicleRackAreaEvents,
+  VehicleRackAreaModel
 > {
   private _vehicle?: Vehicle;
 
-	/**
-	* Create a new ConversationAreaController
-	* @param id
-	* @param topic
-	*/
-	constructor(id: string, vehicle?: Vehicle) {
-		super(id);
-		this._vehicle = vehicle;
-	}
+  /**
+   * Create a new ConversationAreaController
+   * @param id
+   * @param topic
+   */
+  constructor(id: string, vehicle?: Vehicle) {
+    super(id);
+    this._vehicle = vehicle;
+  }
 
   public isActive() {
-		return this.occupants.length > 0;
-	}
+    return this.occupants.length > 0;
+  }
 
   // May need to get rid of this
   protected _updateFrom(newModel: VehicleRackAreaModel): void {
-		// Do I need this?
-	}
+    // Do I need this?
+  }
 
-  set vehicle(vehicle: Vehicle) {
+  set vehicle(vehicle: Vehicle | undefined) {
     // May need to change
-		if (this._vehicle !== vehicle) {
-			this.emit('vehicleChange', vehicle)
-		}
-		this.vehicle = vehicle
-	}
+    if (this._vehicle !== vehicle) {
+      this.emit('vehicleChange', vehicle);
+    }
+    this.vehicle = vehicle;
+  }
 
   get vehicle(): Vehicle | undefined {
-		return this._vehicle;
-	}
+    return this._vehicle;
+  }
 
   public toInteractableAreaModel(): VehicleRackAreaModel {
-		return {
-			id: this.id,
-			occupants: this.occupants.map(player => player.id), //???
-			vehicle: this._vehicle, //Right now model doesnt have this attribute, so may need to remove
-			type: 'VehicleRackArea'
-    }
+    return {
+      id: this.id,
+      occupants: this.occupants.map(player => player.id), //???
+      vehicle: this._vehicle, //Right now model doesnt have this attribute, so may need to remove
+      type: 'VehicleRackArea',
+    };
   }
 }
