5f68ec75418f1dcaf02e5e1a8ddacc57d63e3dac
snavemail <98936310+snavemail@users.noreply.github.com>
2023-11-01 10:32:10 -0400

    Create VehicleRackArea test in backend (issues)

---
+++ townService/src/town/VehicleRackArea.test.ts
@@ -0,0 +1,62 @@
+import { mock, mockClear } from 'jest-mock-extended';
+import { nanoid } from 'nanoid';
+import Player from '../lib/Player';
+import { getLastEmittedEvent } from '../TestUtils';
+import { PlayerID, TownEmitter } from '../types/CoveyTownSocket';
+import VehicleRackArea from './VehicleRackArea';
+import BikeVehicle from './vehicles/BikeVehicle';
+
+describe('VehicleRackArea', () => {
+  const testAreaBox = { x: 100, y: 100, width: 100, height: 100 };
+  let testArea: VehicleRackArea;
+  const townEmitter = mock<TownEmitter>();
+  let newPlayer: Player;
+  const id = nanoid();
+  const occupants: PlayerID[] = [];
+
+  beforeEach(() => {
+    mockClear(townEmitter);
+    testArea = new VehicleRackArea({ id, occupants }, testAreaBox, townEmitter);
+    newPlayer = new Player(nanoid(), mock<TownEmitter>());
+    testArea.add(newPlayer);
+  });
+
+  describe('remove', () => {
+    it('Removes the player from the list of occupants and emits an interactableUpdate event', () => {
+      // Add another player so that we are not also testing what happens when the last player leaves
+      const extraPlayer = new Player(nanoid(), mock<TownEmitter>());
+      testArea.add(extraPlayer);
+      testArea.remove(newPlayer);
+
+      expect(testArea.occupantsByID).toEqual([extraPlayer.id]);
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({
+        id,
+        occupants: [extraPlayer.id],
+        type: 'VehicleRackArea',
+      });
+    });
+  });
+  describe('add', () => {
+    it('Adds the player to the occupants list', () => {
+      expect(testArea.occupantsByID).toEqual([newPlayer.id]);
+    });
+    it('Adds the extra player to the occupants list', () => {
+      const extraPlayer = new Player(nanoid(), mock<TownEmitter>());
+      testArea.add(extraPlayer);
+      expect(testArea.occupantsByID).toEqual([newPlayer.id, extraPlayer.id]);
+    });
+    it('Emits an update for their location', () => {
+      expect(newPlayer.location.interactableID).toEqual(id);
+      const lastEmittedMovement = getLastEmittedEvent(townEmitter, 'playerMoved');
+      expect(lastEmittedMovement.location.interactableID).toEqual(id);
+    });
+  });
+
+  describe('addVehicle', () => {
+    it('Adds the bike to the player', () => {
+      newPlayer.equipVehicle(new BikeVehicle());
+      expect(newPlayer.vehicle).toBeInstanceOf(BikeVehicle);
+    });
+  });
+});
