5f7532af209948c9bbe73d5db8bb356935cf5e9f
snavemail <98936310+snavemail@users.noreply.github.com>
2023-11-01 11:34:45 -0400

    Create empty stub for testing

--- frontend/src/classes/interactable/VehicleRackAreaController.test.ts
+++ frontend/src/classes/interactable/VehicleRackAreaController.test.ts
@@ -1,5 +1,4 @@
-import { mock, mockClear, MockProxy } from 'jest-mock-extended';
-import { nanoid } from 'nanoid';
-import { VehicleRackArea } from '../../types/CoveyTownSocket';
-import TownController from '../TownController';
-import VehicleRackAreaController, { VehicleRackAreaEvents } from './VehicleRackAreaController';
+// Yet to implement
+describe('VehicleRackAreaController', () => {});
+
+export {};
