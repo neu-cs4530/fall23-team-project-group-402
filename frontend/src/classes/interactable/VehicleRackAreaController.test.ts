86ee7664fbe1eb9f3a1b9821c555f73e8f21bbb4
snavemail <98936310+snavemail@users.noreply.github.com>
2023-11-09 17:59:53 -0500

    Finish RackAreaController + Tests

--- frontend/src/classes/interactable/VehicleRackAreaController.test.ts
+++ frontend/src/classes/interactable/VehicleRackAreaController.test.ts
@@ -1,4 +1,133 @@
-// Yet to implement
-describe('VehicleRackAreaController', () => {});
+import { MockProxy, mock, mockClear } from 'jest-mock-extended';
+import { nanoid } from 'nanoid';
+import { PlayerLocation, VehicleType } from '../../types/CoveyTownSocket';
+import PlayerController from '../PlayerController';
+import TownController from '../TownController';
+import VehicleRackAreaController, { VehicleRackAreaEvents } from './VehicleRackAreaController';
 
-export {};
+describe('VehicleRackAreaController', () => {
+  let testArea: VehicleRackAreaController;
+  const townController: MockProxy<TownController> = mock<TownController>();
+  const mockListeners = mock<VehicleRackAreaEvents>();
+  beforeEach(() => {
+    const playerLocation: PlayerLocation = {
+      moving: false,
+      x: 0,
+      y: 0,
+      rotation: 'front',
+    };
+    testArea = new VehicleRackAreaController(nanoid(), townController);
+    testArea.occupants = [
+      new PlayerController(nanoid(), nanoid(), playerLocation, undefined),
+      new PlayerController(nanoid(), nanoid(), playerLocation, undefined),
+      new PlayerController(nanoid(), nanoid(), playerLocation, undefined),
+    ];
+    mockClear(townController);
+    mockClear(mockListeners.vehicleChange);
+    mockClear(mockListeners.occupantsChange);
+    testArea.addListener('vehicleChange', mockListeners.vehicleChange);
+    testArea.addListener('occupantsChange', mockListeners.occupantsChange);
+  });
+  describe('isActive', () => {
+    it('Returns false if the occupants list is empty', () => {
+      testArea.occupants = [];
+      expect(testArea.isActive()).toBe(false);
+    });
+    it('Returns true if the occupants list is not empty', () => {
+      expect(testArea.isActive()).toBe(true);
+    });
+  });
+  describe('set Vehicle', () => {
+    it('does not update the property if the vehicle is the same type', () => {
+      const vehicleCopy = `${testArea.vehicle}`;
+      testArea.vehicle = vehicleCopy as VehicleType;
+      expect(mockListeners.topicChange).not.toBeCalled();
+    });
+    it('sets the vehicle to be a bike', () => {
+      const newVehicle = 'bike';
+      testArea.vehicle = newVehicle;
+      expect(testArea.vehicle).toEqual(newVehicle);
+      expect(testArea.toInteractableAreaModel()).toEqual({
+        id: testArea.id,
+        occupants: testArea.occupants.map(eachOccupant => eachOccupant.id),
+        type: 'VehicleRackArea',
+      });
+    });
+    it('sets the vehicle to be a horse', () => {
+      const newVehicle = 'horse';
+      testArea.vehicle = newVehicle;
+      expect(testArea.vehicle).toEqual(newVehicle);
+      expect(testArea.toInteractableAreaModel()).toEqual({
+        id: testArea.id,
+        occupants: testArea.occupants.map(eachOccupant => eachOccupant.id),
+        type: 'VehicleRackArea',
+      });
+    });
+    it('sets the vehicle to be a skateboard', () => {
+      const newVehicle = 'skateboard';
+      testArea.vehicle = newVehicle;
+      expect(testArea.vehicle).toEqual(newVehicle);
+      expect(testArea.toInteractableAreaModel()).toEqual({
+        id: testArea.id,
+        occupants: testArea.occupants.map(eachOccupant => eachOccupant.id),
+        type: 'VehicleRackArea',
+      });
+    });
+  });
+  describe('equipVehicle', () => {
+    it('equips skateboard to our player', () => {
+      const newVehicle = 'skateboard';
+      testArea.vehicle = newVehicle;
+      testArea.equipVehicle();
+      expect(townController.ourPlayer.vehicle).toEqual({
+        speedMultiplier: 1.5,
+        vehicleType: 'skateboard',
+      });
+    });
+    it('equips bike to our player', () => {
+      const newVehicle = 'bike';
+      testArea.vehicle = newVehicle;
+      testArea.equipVehicle();
+      expect(townController.ourPlayer.vehicle).toEqual({
+        speedMultiplier: 2,
+        vehicleType: 'bike',
+      });
+    });
+    it('equips horse to our player', () => {
+      const newVehicle = 'horse';
+      testArea.vehicle = newVehicle;
+      testArea.equipVehicle();
+      expect(townController.ourPlayer.vehicle).toEqual({
+        speedMultiplier: 3,
+        vehicleType: 'horse',
+      });
+    });
+    it('equips undefined vehicle to our player, (unequips)', () => {
+      const newVehicle = undefined;
+      testArea.vehicle = newVehicle;
+      testArea.equipVehicle();
+      expect(townController.ourPlayer.vehicle).toEqual(undefined);
+    });
+  });
+  describe('setting the occupants property', () => {
+    it('does not update the property if the new occupants are the same set as the old', () => {
+      const origOccupants = testArea.occupants;
+      const occupantsCopy = testArea.occupants.concat([]);
+      const shuffledOccupants = occupantsCopy.reverse();
+      testArea.occupants = shuffledOccupants;
+      expect(testArea.occupants).toEqual(origOccupants);
+      expect(mockListeners.occupantsChange).not.toBeCalled();
+    });
+    it('emits the occupantsChange event when setting the property and updates the model', () => {
+      const newOccupants = testArea.occupants.slice(1);
+      testArea.occupants = newOccupants;
+      expect(testArea.occupants).toEqual(newOccupants);
+      expect(mockListeners.occupantsChange).toBeCalledWith(newOccupants);
+      expect(testArea.toInteractableAreaModel()).toEqual({
+        id: testArea.id,
+        occupants: testArea.occupants.map(eachOccupant => eachOccupant.id),
+        type: 'VehicleRackArea',
+      });
+    });
+  });
+});
--- frontend/src/classes/interactable/VehicleRackAreaController.ts
+++ frontend/src/classes/interactable/VehicleRackAreaController.ts
@@ -1,34 +1,35 @@
+import BikeVehicle from '../../../../townService/src/town/vehicles/BikeVehicle';
+import HorseVehicle from '../../../../townService/src/town/vehicles/HorseVehicle';
+import SkateboardVehicle from '../../../../townService/src/town/vehicles/SkateboardVehicle';
 import {
   Vehicle,
   VehicleRackArea as VehicleRackAreaModel,
   VehicleType,
 } from '../../types/CoveyTownSocket';
+import TownController from '../TownController';
 import InteractableAreaController, { BaseInteractableEventMap } from './InteractableAreaController';
 
 /**
  * The events that a ViewingAreaController can emit
  */
-export type VehicleRackAreaEvents = BaseInteractableEventMap & {
-  /**
-   * A vehicleChange event indicates that the vehicle selected for this vehicle rack area has changed.
-   * Must implement Vehicle type to
-   */
-  vehicleChange: (vehicle: VehicleType | undefined) => void;
-};
+export type VehicleRackAreaEvents = BaseInteractableEventMap;
 
 export default class VehicleRackAreaController extends InteractableAreaController<
   VehicleRackAreaEvents,
   VehicleRackAreaModel
 > {
-  private _vehicle?: Vehicle;
+  private _vehicle?: VehicleType;
+
+  protected _townController: TownController;
 
   /**
    * Create a new ConversationAreaController
    * @param id
    * @param topic
    */
-  constructor(id: string, vehicle?: Vehicle) {
+  constructor(id: string, townController: TownController, vehicle?: VehicleType) {
     super(id);
+    this._townController = townController;
     this._vehicle = vehicle;
   }
 
@@ -36,34 +37,50 @@ export default class VehicleRackAreaController extends InteractableAreaControlle
     return this.occupants.length > 0;
   }
 
-  protected _updateFrom(): void {
-    // Do I need this?
-  }
+  protected _updateFrom(): void {}
 
   /**
    * Set vehicle to undefined when selecting no vehicle
    */
-  set vehicle(vehicle: Vehicle | undefined) {
-    // May need to change
-    if (this._vehicle !== vehicle) {
-      this.emit('vehicleChange', vehicle);
-    }
-    this.vehicle = vehicle;
+  set vehicle(vehicle: VehicleType | undefined) {
+    this._vehicle = vehicle;
   }
 
   /**
    * Get vehicle will return undefined when there is no vehicle selected
    * OR when the player selects the no vehicle option
    */
-  get vehicle(): Vehicle | undefined {
+  get vehicle(): VehicleType | undefined {
     return this._vehicle;
+  }
+
+  public equipVehicle(): void {
+    let vehicle: Vehicle | undefined = undefined;
+    switch (this._vehicle) {
+      case 'bike':
+        vehicle = new BikeVehicle();
+        break;
+      case 'horse':
+        vehicle = new HorseVehicle();
+        break;
+
+      case 'skateboard':
+        vehicle = new SkateboardVehicle();
+        break;
+      case undefined:
+        vehicle = undefined;
+        break;
+      default:
+        vehicle = undefined;
+        break;
+    }
+    this._townController.ourPlayer.vehicle = vehicle;
   }
 
   public toInteractableAreaModel(): VehicleRackAreaModel {
     return {
       id: this.id,
       occupants: this.occupants.map(player => player.id), //???
-      vehicle: this._vehicle, //Right now model doesnt have this attribute, so may need to remove
       type: 'VehicleRackArea',
     };
   }
