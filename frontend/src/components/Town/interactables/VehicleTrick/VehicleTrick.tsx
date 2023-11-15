7289ade539a9995e0121114ffcbe44b4dbbca726
AbhayBishts <bishts.abhay+github@gmail.com>
2023-11-15 14:07:27 -0500

    Changed function comments to reflect new game

--- frontend/src/components/Town/interactables/VehicleTrick/VehicleTrick.tsx
+++ frontend/src/components/Town/interactables/VehicleTrick/VehicleTrick.tsx
@@ -7,22 +7,19 @@ export type VehicleTrickGameProps = {
 };
 
 /**
- * A component that renders the TicTacToe board
+ * A component that renders the VehicleTrick game
  *
- * Renders the TicTacToe board as a "StyledTicTacToeBoard", which consists of 9 "StyledTicTacToeSquare"s
- * (one for each cell in the board, starting from the top left and going left to right, top to bottom).
- * Each StyledTicTacToeSquare has an aria-label property that describes the cell's position in the board,
- * formatted as `Cell ${rowIndex},${colIndex}`.
+ * Renders a timer (which immediately begins counting down), the current word that needs to be typed,
+ * and an input field that is automatically selected. The input field only accepts letters.
  *
- * The board is re-rendered whenever the board changes, and each cell is re-rendered whenever the value
- * of that cell changes.
+ * The timer is rerendered each second to represent the countdown, the input field rerenders each time
+ * the player types in a letter, and the word displayed rerenders whenever the user spells it correctly.
  *
- * If the current player is in the game, then each StyledTicTacToeSquare is clickable, and clicking
- * on it will make a move in that cell. If there is an error making the move, then a toast will be
- * displayed with the error message as the description of the toast. If it is not the current player's
- * turn, then the StyledTicTacToeSquare will be disabled.
+ * Once the timer runs out, the display automatically changes to display the user's score, an 3-letter
+ * input field for their username, and a submit button. The input field only accepts letter inputs with
+ * a length of three characters.
  *
- * @param gameAreaController the controller for the TicTacToe game
+ * @param gameAreaController the controller for the VehicleTrick game
  */
 export default function VehicleTrick({ gameAreaController }: VehicleTrickGameProps): JSX.Element {
   const [input, setInput] = useState<string>('');
