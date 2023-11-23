af468129013ca5c574924c599d98aac02ef91683
AbhayBishts <bishts.abhay+github@gmail.com>
2023-11-22 21:54:43 -0500

    Fixed lint error

--- frontend/src/components/Town/interactables/VehicleTrick/VehicleTrick.test.tsx
+++ frontend/src/components/Town/interactables/VehicleTrick/VehicleTrick.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import VehicleTrick from './VehicleTrick';
 import VehicleTrickAreaController from '../../../../classes/interactable/VehicleTrickAreaController';
 import { mock } from 'jest-mock-extended';
