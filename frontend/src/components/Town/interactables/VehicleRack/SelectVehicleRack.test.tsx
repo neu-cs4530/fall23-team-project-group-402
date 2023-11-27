a4a21ad819cf902036dc4d4dea6bb96036001162
femailman <93400575+femailman@users.noreply.github.com>
2023-11-26 20:42:20 -0500

    Initial commit for testing Select Vehicle Rack

---
+++ frontend/src/components/Town/interactables/VehicleRack/SelectVehicleRack.test.tsx
@@ -0,0 +1,54 @@
+import ChakraProvider from '@chakra-ui/react';
+import { fireEvent, render, screen, waitFor, within } from '@testing-library/react';
+import { mock, mockReset } from 'jest-mock-extended';
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { act } from 'react-dom/test-utils';
+import VehicleTrickAreaController from '../../../../classes/interactable/VehicleTrickAreaController';
+import PlayerController from '../../../../classes/PlayerController';
+import TownController, * as TownControllerHooks from '../../../../classes/TownController';
+import TownControllerContext from '../../../../contexts/TownControllerContext';
+import PhaserGameArea from '../GameArea';
+import VehicleRackAreaController from '../../../../classes/interactable/VehicleRackAreaController';
+import { PlayerLocation } from '../../../../types/CoveyTownSocket';
+
+const mockToast = jest.fn();
+jest.mock('@chakra-ui/react', () => {
+  const ui = jest.requireActual('@chakra-ui/react');
+  const mockUseToast = () => mockToast;
+  return {
+    ...ui,
+    useToast: mockUseToast,
+  };
+});
+
+const randomLocation = (): PlayerLocation => ({
+  moving: Math.random() < 0.5,
+  rotation: 'front',
+  x: Math.random() * 1000,
+  y: Math.random() * 1000,
+});
+
+class MockVehicleRackAreaController extends VehicleRackAreaController {}
+
+describe('VehicleRackArea', () => {
+  // Spy on console.error and intercept react key warnings to fail test
+  let consoleErrorSpy: jest.SpyInstance<void, [message?: any, ...optionalParms: any[]]>;
+  beforeAll(() => {
+    // Spy on console.error and intercept react key warnings to fail test
+    consoleErrorSpy = jest.spyOn(global.console, 'error');
+    consoleErrorSpy.mockImplementation((message?, ...optionalParams) => {
+      const stringMessage = message as string;
+      if (stringMessage.includes && stringMessage.includes('children with the same key,')) {
+        throw new Error(stringMessage.replace('%s', optionalParams[0]));
+      } else if (stringMessage.includes && stringMessage.includes('warning-keys')) {
+        throw new Error(stringMessage.replace('%s', optionalParams[0]));
+      }
+      // eslint-disable-next-line no-console -- we are wrapping the console with a spy to find react warnings
+      console.warn(message, ...optionalParams);
+    });
+  });
+  afterAll(() => {
+    consoleErrorSpy.mockRestore();
+  });
+});
